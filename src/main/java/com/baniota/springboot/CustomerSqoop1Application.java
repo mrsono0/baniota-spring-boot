36b87e590edffd664439583bcc964ababd91e099
Jung Hyun Son <mrsono0@gmail.com>
2018-07-30 23:47:38 +0900

    no message

---
+++ src/main/java/com/baniota/springboot/CustomerSqoop1Application.java
@@ -0,0 +1,12 @@
+package com.baniota.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSqoop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSqoop1Application.class, args);
+	}
+}
---
+++ src/test/java/com/baniota/springboot/CustomerSqoop1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baniota.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerSqoop1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
